355670bc3c7ff93f3e83e87deb4cbff11b78b801
fridayy <bnjm@harmless.ninja>
2018-10-22 21:29:52 +0200

    renamed builder to avoid variable shadowing

--- dynamodb-backup/src/main/java/one/leftshift/backup/service/task/TableBackupRequest.java
+++ dynamodb-backup/src/main/java/one/leftshift/backup/service/task/TableBackupRequest.java
@@ -19,11 +19,11 @@ public class TableBackupRequest {
     private final AmazonDynamoDB dynamoDBClient;
     private final List<BackupPreprocessor> preprocessors;
 
-    private TableBackupRequest(Builder builder) {
-        this.tableName = builder.tableName;
-        this.initialRequest = builder.initialRequest;
-        this.dynamoDBClient = builder.dynamoDBClient;
-        this.preprocessors = builder.preprocessors;
+    private TableBackupRequest(BuilderImpl builderImpl) {
+        this.tableName = builderImpl.tableName;
+        this.initialRequest = builderImpl.initialRequest;
+        this.dynamoDBClient = builderImpl.dynamoDBClient;
+        this.preprocessors = builderImpl.preprocessors;
     }
 
     String getTableName() {
@@ -42,41 +42,41 @@ public class TableBackupRequest {
         return Collections.unmodifiableList(preprocessors);
     }
 
-    public static Builder builder() {
-        return new Builder();
+    public static BuilderImpl builder() {
+        return new BuilderImpl();
     }
 
-    public static class Builder implements one.leftshift.common.Builder<TableBackupRequest> {
+    public static class BuilderImpl implements one.leftshift.common.Builder<TableBackupRequest> {
         private String tableName;
         private BackupRequest initialRequest;
         private AmazonDynamoDB dynamoDBClient;
         private List<BackupPreprocessor> preprocessors = new ArrayList<>();
 
-        public Builder tableName(String tableName) {
+        public BuilderImpl tableName(String tableName) {
             Objects.requireNonNull(tableName, "tableName can not be null");
             this.tableName = tableName;
             return this;
         }
 
-        public Builder initialRequest(BackupRequest initialRequest) {
+        public BuilderImpl initialRequest(BackupRequest initialRequest) {
             Objects.requireNonNull(initialRequest, "initialRequest can not be null");
             this.initialRequest = initialRequest;
             return this;
         }
 
-        public Builder dynamoDBClient(AmazonDynamoDB dynamoDBClient) {
+        public BuilderImpl dynamoDBClient(AmazonDynamoDB dynamoDBClient) {
             Objects.requireNonNull(dynamoDBClient, "dynamoDBClient can not be null");
             this.dynamoDBClient = dynamoDBClient;
             return this;
         }
 
-        public Builder withPreprocessor(BackupPreprocessor preprocessor) {
+        public BuilderImpl withPreprocessor(BackupPreprocessor preprocessor) {
             Objects.requireNonNull(dynamoDBClient, "preprocessor can not be null");
             this.preprocessors.add(preprocessor);
             return this;
         }
 
-        public Builder withPreprocessors(List<BackupPreprocessor> preprocessors) {
+        public BuilderImpl withPreprocessors(List<BackupPreprocessor> preprocessors) {
             Objects.requireNonNull(dynamoDBClient, "preprocessors can not be null");
             this.preprocessors.addAll(preprocessors);
             return this;
